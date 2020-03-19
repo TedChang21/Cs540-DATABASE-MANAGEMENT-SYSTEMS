793338d23bed37297321f156af15a695fa3f753e
CheKai Chang <chanchek@flip2.engr.oregonstate.edu>
2020-03-19 14:46:02 -0700

    finish version

---
+++ FinalProject/index.php
@@ -0,0 +1,128 @@
+<?php 
+$dbhost = 'oniddb.cws.oregonstate.edu';
+$dbname = 'chanchek-db';
+$dbuser = 'chanchek-db';
+$dbpass = 'A8sePfr93XF1Jp1A';
+$link = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+function died($error) {
+    echo '<script type="text/javascript"> alert("'.$error.'")</script>';
+    die();
+}
+if (isset($_POST["submit"])) {
+  
+  $WineInformation1 = $_POST["WineInformation1"];
+
+  $attribute1 = $_POST["customselect1"];
+  $order1=$_POST["order1"];  
+  $query1 = "SELECT distinct WD.WineID, WD.points,WD.price,WD.winery,WD.region1,WD.province,WD.country FROM `wine_data_1` WD,`wine_ranking` WK WHERE WD.$attribute1= '$WineInformation1' and WD.winery= WK.winery ORDER BY WD.$order1 DESC LIMIT 0 , 30";
+  $query2 = "SELECT distinct WD.WineID, WD.points,WD.price,WD.winery,WD.region1,WD.province,WD.country FROM `wine_data_2` WD,`wine_ranking` WK WHERE WD.$attribute1 = '$WineInformation1' and WD.winery= WK.winery ORDER BY WD.$order1 DESC LIMIT 0 , 30";
+  $query3 = "SELECT distinct WD.WineID, WD.points,WD.price,WD.winery,WD.region1,WD.province,WD.country FROM `wine_data_1` WD WHERE WD.$attribute1 = '$WineInformation1' and WD.WineID not in (SELECT WD1.WineID FROM `wine_data_1` WD1,`wine_ranking` WK WHERE WD.$attribute1= '$WineInformation1' and WD1.winery= WK.winery) ORDER BY WD.$order1 DESC LIMIT 0 , 30";
+  $query4 = "SELECT distinct WD.WineID, WD.points,WD.price,WD.winery,WD.region1,WD.province,WD.country FROM `wine_data_2` WD WHERE WD.$attribute1 = '$WineInformation1' and WD.WineID not in (SELECT WD1.WineID FROM `wine_data_2` WD1,`wine_ranking` WK WHERE WD.$attribute1= '$WineInformation1' and WD1.winery= WK.winery) ORDER BY WD.$order1 DESC LIMIT 0 , 30";
+
+  
+  
+  
+  $data1 = mysqli_query($link, $query1); 
+  $data2 = mysqli_query($link, $query2); 
+  $data3 = mysqli_query($link, $query3); 
+  $data4 = mysqli_query($link, $query4);
+}
+
+
+?>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<style>
+body {
+  background-image: url('back.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+<body>
+<table width="700" border="1" align="center" style="color:white;">
+   <tr>
+    <!-- <td >WineID</td> -->
+    <td >points</td>
+    <td >price</td>
+    <td >winery</td>
+    <td >regionl</td>
+    <td >province</td>
+    <td >country</td>
+    <td >Winery In 2019 top 100</td>
+  </tr>
+</body>
+<?php
+header("Content-Type:text/html; charset=utf-8");
+  for($i=1;$i<=mysqli_num_rows($data1);$i++){
+$rs1=mysqli_fetch_row($data1);
+?>
+  <tr>
+    <td><?php echo $rs1[1]?></td>
+    <td><?php echo $rs1[2]?></td>
+    <td><?php echo $rs1[3]?></td>
+    <td><?php echo $rs1[4]?></td>
+    <td><?php echo $rs1[5]?></td>
+    <td><?php echo $rs1[6]?></td>
+    <td><?php echo "<a href='top_100.php?id=".$rs1[0]."' target='_blank' >Yes</a>"?></td>
+  </tr>
+  
+<?php
+}
+?>
+<?php
+  for($i=1;$i<=mysqli_num_rows($data2);$i++){
+$rs2=mysqli_fetch_row($data2);
+?>
+  <tr>
+    <td><?php echo $rs2[1]?></td>
+    <td><?php echo $rs2[2]?></td>
+    <td><?php echo $rs2[3]?></td>
+    <td><?php echo $rs2[4]?></td>
+    <td><?php echo $rs2[5]?></td>
+    <td><?php echo $rs2[6]?></td>
+    <td><?php echo "<a href='top_100.php?id=".$rs2[0]."' target='_blank'>Yes</a>"?></td>
+  </tr>
+  
+<?php
+}
+?>
+<?php
+  for($i=1;$i<=mysqli_num_rows($data3);$i++){
+$rs3=mysqli_fetch_row($data3);
+?>
+  <tr>
+    <td><?php echo $rs3[1]?></td>
+    <td><?php echo $rs3[2]?></td>
+    <td><?php echo $rs3[3]?></td>
+    <td><?php echo $rs3[4]?></td>
+    <td><?php echo $rs3[5]?></td>
+    <td><?php echo $rs3[6]?></td>
+    <td><?php echo "No"?></td>
+  </tr>
+  
+<?php
+}
+?>
+<?php
+  for($i=1;$i<=mysqli_num_rows($data4);$i++){
+$rs4=mysqli_fetch_row($data4);
+?>
+  <tr>
+    <td><?php echo $rs4[1]?></td>
+    <td><?php echo $rs4[2]?></td>
+    <td><?php echo $rs4[3]?></td>
+    <td><?php echo $rs4[4]?></td>
+    <td><?php echo $rs4[5]?></td>
+    <td><?php echo $rs4[6]?></td>
+    <td><?php echo "No"?></td>
+  </tr>
+  
+<?php
+}
+?>
---
+++ FinalProject/top_100.php
@@ -0,0 +1,100 @@
+<?php 
+$dbhost = 'oniddb.cws.oregonstate.edu';
+$dbname = 'chanchek-db';
+$dbuser = 'chanchek-db';
+$dbpass = 'A8sePfr93XF1Jp1A';
+$link = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<style>
+body {
+  background-image: url('top.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+	<head>
+    <title>Search Wine Online | Consign and Search Your Wine Collection on 540DB.com | WineAid</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width,initial-scale=1" />
+		<link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="css/top_100.css?v=1.44" rel="stylesheet"/>
+			
+	</head>
+	<body>
+		
+
+		<div class="intro-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="intro-message" align="center">
+						<a href="http://web.engr.oregonstate.edu/~chanchek/index.html"><h1><font color="#006699">Top Wine</font></h1></a>
+
+						<hr class="intro-divider" />
+							<div class="input-group">
+								<?php
+                
+										// $wineid = $_GET["id"];
+										$query1 = "SELECT WK.rank,WK.winery, WK.wine, WK.description, count(WK.winery) FROM `wine_ranking111` WK WHERE  WK.rank<50 group by WK.rank LIMIT 0 , 50";
+										$query2 = "SELECT WK.rank,WK.winery, WK.wine, WK.description, count(WK.winery) FROM `wine_ranking111` WK WHERE  WK.rank>50 group by WK.rank LIMIT 0 , 50";
+										$data1 = mysqli_query($link, $query1);
+										$data2 = mysqli_query($link, $query2);
+										/*if (mysqli_num_rows($data1) == 0&&mysqli_num_rows($data2) == 0)
+										{
+											echo "No Result";
+											exit;
+										}*/
+								?>
+                
+								<table width="700" border="1" align="left">
+								<tr>
+									<td >Rank</td>
+									<td >Winery</td>
+									<td >Wine</td>
+									<td >description</td>
+									<td >Number of wine in searching system</td>
+								</tr>
+								<?php
+									for($i=1;$i<=100;$i++){
+										$rs1=mysqli_fetch_row($data1);
+								?>
+									<tr>
+										<td><?php echo $rs1[0]?></td>
+										<td><?php echo $rs1[1]?></td>
+										<td><?php echo $rs1[2]?></td>
+										<td><?php echo $rs1[3]?></td>
+										<td><?php echo $rs1[4]?></td>
+									</tr>
+								<?php
+									}
+								?>
+								<?php
+									for($i=1;$i<=100;$i++){
+										$rs2=mysqli_fetch_row($data2);
+								?>
+									<tr>
+										<td><?php echo $rs2[0]?></td>
+										<td><?php echo $rs2[1]?></td>
+										<td><?php echo $rs2[2]?></td>
+										<td><?php echo $rs2[3]?></td>
+										<td><?php echo $rs2[4]?></td>
+									</tr>
+								<?php
+									}
+								?>
+						<!-- &nbsp; -->
+						<!-- <div id="msg"></div> -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<!-- <script src="index.php"></script> -->
+</html>
